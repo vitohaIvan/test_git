39c4477124aae3d9fc3bc7c212659ba0cdb24f35
vitohaIvan <ivanvitoshko@yandex.com>
2026-09-17 20:56:56 +0300

    new update

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello World')
+nums = [1,2,3,1]
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        seen = set()
+        for i in nums:
+            if i in seen:
+                return True
+            seen.add(i)
+        return False
